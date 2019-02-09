809fb7886e103f020b5f6d0e2d9adcb4acba7e63
Genadi Mihov <gmihov7@gmail.com>
2019-02-09 19:48:18 +0200

    fixed

--- Simple-Calculations/square-area.js
+++ Simple-Calculations/square-area.js
@@ -1,6 +1,6 @@
 function getSquareArea(input) {
-    let side = Number(a);
-    let area = side * side;
+    let side = Number(input[0]);
+    let area = Math.pow(side, 2);
 
     console.log(area);
 }
