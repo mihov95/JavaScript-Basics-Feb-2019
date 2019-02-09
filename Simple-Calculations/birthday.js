a44eb03219dcfea7c3a6b09c304755fadebe1e04
Genadi Mihov <gmihov7@gmail.com>
2019-02-09 21:11:37 +0200

    added birthday.js

---
+++ Simple-Calculations/birthday.js
@@ -0,0 +1,16 @@
+function birthday(input) {
+    let lenght = Number(input[0]);
+    let widght = Number(input[1]);
+    let h = Number(input[2]);
+    let percent = Number(input[3]);
+
+    let volume = lenght * widght * h;
+    let allLitres= volume * 0.001;
+    percent = 17*0.01;
+    let result = allLitres*(1-percent);
+
+    console.log(result.toFixed(3));
+    
+}
+
+birthday(['85','75','47','17']);
