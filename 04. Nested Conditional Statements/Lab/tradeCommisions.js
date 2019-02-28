4f659675a119b79f954f0de8a7f68fe63ef06816
Genadi <gmihov7@gmail.com>
2019-02-28 19:32:53 +0200

    fixed

--- 04. Nested Conditional Statements/Lab/tradeCommisions.js
+++ 04. Nested Conditional Statements/Lab/tradeCommisions.js
@@ -2,7 +2,7 @@ function tradeCommisions(input) {
     let city = input[0];
     let sales = Number(input[1]);
 
-    let commision = 0.00;
+    let commision = -1;
 
     switch (city) {
         case "Sofia":
@@ -18,8 +18,6 @@ function tradeCommisions(input) {
                 commision = sales * 0.12;
                 
             }
-            console.log(commision.toFixed(2));
-
             break;
         case "Varna":
             if (sales >= 0 && sales <= 500) {
@@ -34,8 +32,6 @@ function tradeCommisions(input) {
                 commision = sales * 0.13;
                 
             }
-            console.log(commision.toFixed(2));
-
             break;
         case "Plovdiv":
             if (sales >= 0 && sales <= 500) {
@@ -49,17 +45,20 @@ function tradeCommisions(input) {
             } else if (sales > 10000) {
                 commision = sales * 0.145;
                 
-            }
-            console.log(commision.toFixed(2));
-            
+            }           
             break;
     
         default:
             console.log("error");
-            
             break;
     }
 
-}
+    if (commision < 0) {
+        console.log("error");
+        
+    } else {
+        console.log(commision.toFixed(2));
 
-tradeCommisions(["Plovdiv", 1500]);
+    }
+
+}
