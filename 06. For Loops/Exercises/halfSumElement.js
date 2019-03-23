9f704e15f5b333a5188d155e3a573c3e9d57027a
mihov95 <gmihov7@gmail.com>
2019-03-23 09:18:55 +0200

    added

--- 06. For Loops/Exercises/halfSumElement.js
+++ 06. For Loops/Exercises/halfSumElement.js
@@ -2,30 +2,39 @@ function halfSumElement(input) {
     let n = Number(input.shift());
     let sum = 0;
     let currNum = 0;
+    let max = Number.MIN_SAFE_INTEGER
 
     for (let i = 0; i < n; i++) {
         currNum = Number(input.shift());
         sum += currNum;
+        
+        if (currNum > max) {
+            max = currNum;
+        }
 
         
     }
-            if (currNum == (sum / 2)) {
-                console.log(`Yes`);
-                console.log(`Sum = ${currNum}`);
-                
-            } else {
+
+    
+    if (max == (sum / 2)) {
+        console.log(`Yes`);
+        console.log(`Sum = ${max}`);
+        
+    } else {
+        sum -= max;
                 console.log(`No`);
-                let diff = currNum - sum;
-                console.log(`Diff = ${diff}`);
+                let diff = max - sum;
+                console.log(`Diff = ${Math.abs(diff)}`);
     
                 
             }
 }
 
 halfSumElement([
-    3,
-5,
-5,
-1
-
+    4,
+    6,
+    1,
+    2,
+    3
+    
 ])
