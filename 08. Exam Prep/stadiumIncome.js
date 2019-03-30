cc5ce5b9b8e79809030a19940755826fbad0369b
mihov95 <gmihov7@gmail.com>
2019-03-30 15:14:55 +0200

    updated

--- 08. Exam Prep/stadiumIncome.js
+++ 08. Exam Prep/stadiumIncome.js
@@ -1,3 +1,15 @@
 function stadiumIncome(input) {
-    
+    let countSectors = Number(input.shift());
+    let stadiumCapacity = Number(input.shift());
+    let ticketPrice = Number(input.shift());
+
+    let income = stadiumCapacity * ticketPrice;
+    let sectorIncome = income / countSectors;
+
+    let charityMoney = (income - (sectorIncome * 0.75)) / 8;
+
+    console.log(`Total income - ${income.toFixed(2)} BGN`);
+    console.log(`Money for charity - ${charityMoney.toFixed(2)} BGN`);
 }
+
+stadiumIncome([4,5000,5])
